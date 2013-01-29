463e161b30052d279ec7c79bc3e621f3c96af3e4
Elliott Foster <elliottf@codebrews.com>
2013-01-29 17:53:07 -0600

    Playbook with solr, mongo, node, and fun stuff.

--- vagrant/provision/dev-setup.sh
+++ vagrant/provision/dev-setup.sh
@@ -5,6 +5,6 @@ DIR='/usr/src'
 
 echo ''
 echo '======================================================================='
-echo 'Setting up the Training Wheels developer environment...'
+echo 'Setting up the local developer environment...'
 cd $DIR/vagrant/provision
 ansible-playbook -c local --user=root dev-setup-playbook.yml
