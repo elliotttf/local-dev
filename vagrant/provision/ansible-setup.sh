be72fc7a7f58b3b6b7dc7ad505fb4cefda324022
Elliott Foster <elliottf@codebrews.com>
2013-08-15 11:29:23 -0500

    Switch back to ansible PPA and break out mysql ib* removals.

--- vagrant/provision/ansible-setup.sh
+++ vagrant/provision/ansible-setup.sh
@@ -27,19 +27,10 @@ python-pip debhelper python-support cdbs > /dev/null 2>&1 || error_exit "Unable 
 
 echo ''
 echo '======================================================================='
-echo 'Checking out Ansible...'
-mkdir /usr/local/src/ansible || error_exit "Unable to create the directory for Ansible."
-git clone https://github.com/ansible/ansible.git /usr/local/src/ansible > /dev/null || error_exit "Unable to clone the git repository for Ansible."
-cd /usr/local/src/ansible
-git checkout v0.9 > /dev/null 2>&1 || error_exit "Unable to checkout the required tag."
-
-echo ''
-echo '======================================================================='
-echo 'Building Ansible .deb file...'
-make deb > /dev/null 2>&1 || error_exit "Unable to build the .deb file."
-cd /usr/local/src
-dpkg -i ansible_0.9_all.deb > /dev/null || error_exit "Unable to build the .deb file."
-rm ansible_0.9_all.deb
+echo 'Downloading Ansible...'
+add-apt-repository ppa:rquillo/ansible
+aptitude -q=2 update > /dev/null || error_exit "Unable to update apt cache."
+aptitude -q=2 -y install ansible > /dev/null 2>&1 || error_exit "Unable to install required packages."
 
 echo "localhost" > /etc/ansible/hosts
 
